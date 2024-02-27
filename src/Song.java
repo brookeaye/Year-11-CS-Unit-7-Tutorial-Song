7fe8d325c0587cfec42efe66142c140a9db00696
brookeaye <brookey2026@student.cis.edu.hk>
2024-02-27 09:39:33 +0800

    This was a lot easier than Domino

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    private ArrayList<String> listeners = new ArrayList<>();
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public int howMany(ArrayList <String> listeners){
+        int newListeners = 0;
+        for (String listener : listeners) {
+            listener = listener.toLowerCase();
+            if (!this.listeners.isEmpty() && this.listeners.contains(listener)) {
+                continue;
+            }
+            this.listeners.add(listener);
+            newListeners++;
+        }
+        return newListeners;
+    }
 }
